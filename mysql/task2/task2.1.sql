9bfb56481ff2dfcb6aea94c46b6f6be1602a19d9
Serhii Zashchik <zachik13@gmail.com>
2019-03-11 19:44:12 +0200

    Unique pair added

--- mysql/task2/task2.1.sql
+++ mysql/task2/task2.1.sql
@@ -6,3 +6,14 @@ INSERT INTO product_descriptions (id, product_id, lang, name, description)
 VALUES (5, 5, "eng", "Microsoft keyboard", "Microsoft keyboard altered"); 
 ON DUPLICATE KEY UPDATE description=VALUES(description), lang=VALUES(lang), name=VALUES(name);
 
+//If primary або unique key already exists "on duplicate key update" part of command is executed.
+//In this case id (not the best example) is duplicated.
+
+//Its better make product_id and lang an unique couple.
+
+ALTER TABLE product_descriptions ADD CONSTRAINT id_lang UNIQUE (product_id, lang);
+
+INSERT INTO product_descriptions (product_id, lang, name, description) 
+VALUES (5, "eng", "Microsoft keyboard", "Microsoft keyboard altered 2"); 
+ON DUPLICATE KEY UPDATE description=VALUES(description), lang=VALUES(lang), name=VALUES(name);
+
